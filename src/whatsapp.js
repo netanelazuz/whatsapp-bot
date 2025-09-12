2934e31c62405230bac1e908b35f15f249e463dd
netanel azuz <netanelazuz@gmail.com>
2025-09-12 18:27:18 +0300

    added puppeteer

--- src/whatsapp.js
+++ src/whatsapp.js
@@ -6,8 +6,19 @@ const { Client, LocalAuth } = pkg;
 
 const client = new Client({
   authStrategy: new LocalAuth(),
-  puppeteer: { headless: true }
+  puppeteer: {
+    headless: true,
+    args: [
+      "--no-sandbox",
+      "--disable-setuid-sandbox",
+      "--disable-dev-shm-usage",
+      "--disable-gpu",
+      "--no-zygote",
+      "--single-process"
+    ]
+  }
 });
+
 
 client.on("qr", qr => {
   qrcode.generate(qr, { small: true });
